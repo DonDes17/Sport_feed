659d8221c3a8448128f61dfa2b90856603fcc112
e-noumene <e.noumene@gmail.com>
2019-01-16 21:55:37 +0100

    add navigation & create Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
-import Home from './pages/Home';
+
+import  Home  from './pages/Home';
+import Details from './pages/Details';
 import Page404 from './pages/Page404';
 
 
@@ -14,21 +17,29 @@ class App extends Component {
     const { activeItem } = this.state
     return (
       <div className="App">
-         <div>
-          <Menu className="large inverted pointing secondary" style={{backgroundColor: 'black'}}> >
-            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-            <Menu.Item
-              name='messages'
-              active={activeItem === 'messages'}
+         <div >
+          <Menu className="large inverted pointing secondary" style={{backgroundColor: 'black', height: '30% !important'}}> 
+            <Menu.Item 
+              name='home' 
+              active={activeItem === 'home'} 
               onClick={this.handleItemClick}
-            />
+              >
+              <Link to="/">Home</Link>
+            </Menu.Item>
             <Menu.Item
-              name='friends'
-              active={activeItem === 'friends'}
+              name='détails'
+              active={activeItem === 'détails'}
               onClick={this.handleItemClick}
-            />
+            >
+              <Link to="/details">Détails</Link>
+            </Menu.Item>
           </Menu>
         </div> 
+        <Switch>
+        <Route exact path='/' component={Home} />
+        <Route  path='/details' component={Details} />
+        <Route component={Page404} />
+        </Switch>
       </div>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react'
+
+
+import logo from '../images/logo.png';
+
+const Home = () => (
+
+  <div style={{backgroundColor: 'black', height: '100vh', width: '100vw', display: 'flex', justifyContent:'center', alignItems: 'flex-start', paddingTop: '5%'}}>
+    <div style={{display: 'flex', flexDirection: 'column',heigh: '50%', width: '100%', justifyContent: 'center', alignItems: 'center'}}>
+    <div style={{backgroundImage: `url(${logo})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition:'center' , height: '45vh', width: '35vw'}}>
+    </div>
+    <Button style={{backgroundColor: '#dfbb4c',color: 'white', textAlign: 'center', marginTop: '5%'}}>ENTRER</Button>
+    </div>
+  </div>
+ 
+)
+
+
+export default Home;
