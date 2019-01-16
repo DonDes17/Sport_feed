573c3d72b804b676aa680f1e41422ed5e3277b68
e-noumene <e.noumene@gmail.com>
2019-01-16 13:46:48 +0100

    add Pgae404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react';
+
+import Home from './pages/Home';
+import Page404 from './pages/Page404';
 
 
 class App extends Component {
@@ -11,21 +14,21 @@ class App extends Component {
     const { activeItem } = this.state
     return (
       <div className="App">
-          <div>
-        <Menu pointing secondary >
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item
-            name='messages'
-            active={activeItem === 'messages'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='friends'
-            active={activeItem === 'friends'}
-            onClick={this.handleItemClick}
-          />
-        </Menu>
-      </div>
+         <div>
+          <Menu className="large inverted pointing secondary" style={{backgroundColor: 'black'}}> >
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+            <Menu.Item
+              name='messages'
+              active={activeItem === 'messages'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='friends'
+              active={activeItem === 'friends'}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
+        </div> 
       </div>
     );
   }
---
+++ src/pages/Page404.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/page404.css'
+
+const Page404 = () => (
+  <div id="notfound">
+		<div className="notfound">
+			<div className="notfound-404">
+				<h1>Oops!</h1>
+				<h2>404 - La page n'a pas été trouvée</h2>
+			</div>
+			<a href="#">revenir à Home</a>
+		</div>
+	</div>
+)
+
+
+
+export default Page404;
