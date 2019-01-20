32b67e80aeb38dca3bb3f7d648cc7523cc079417
e-noumene <e.noumene@gmail.com>
2019-01-21 00:39:00 +0100

    remove store provider

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import { Provider }  from 'react-redux';
 
 import App from './App';
-import Store from './redux/configStore';
+
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  <Provider store={Store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
   , 
 document.getElementById('root'));
 
